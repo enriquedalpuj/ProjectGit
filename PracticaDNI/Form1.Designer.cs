7b500e6944a1d38ba8d17469675a93f0d41bcf47
ENRIQUE DALMAU PUJOL <enriquedalpuj@monlau.com>
2020-02-05 16:40:53 +0100

    Version 0.0.1.

---
+++ PracticaDNI/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PracticaDNI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxNum = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLetter = new System.Windows.Forms.TextBox();
+            this.buttonConvert = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxNum
+            // 
+            this.textBoxNum.Location = new System.Drawing.Point(196, 43);
+            this.textBoxNum.Name = "textBoxNum";
+            this.textBoxNum.Size = new System.Drawing.Size(254, 20);
+            this.textBoxNum.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(40, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(150, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Escribe DNI sin letra";
+            // 
+            // textBoxLetter
+            // 
+            this.textBoxLetter.Location = new System.Drawing.Point(456, 43);
+            this.textBoxLetter.Name = "textBoxLetter";
+            this.textBoxLetter.ReadOnly = true;
+            this.textBoxLetter.Size = new System.Drawing.Size(35, 20);
+            this.textBoxLetter.TabIndex = 2;
+            // 
+            // buttonConvert
+            // 
+            this.buttonConvert.Location = new System.Drawing.Point(232, 81);
+            this.buttonConvert.Name = "buttonConvert";
+            this.buttonConvert.Size = new System.Drawing.Size(127, 36);
+            this.buttonConvert.TabIndex = 3;
+            this.buttonConvert.Text = "Convert";
+            this.buttonConvert.UseVisualStyleBackColor = true;
+            this.buttonConvert.Click += new System.EventHandler(this.buttonConvert_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(614, 157);
+            this.Controls.Add(this.buttonConvert);
+            this.Controls.Add(this.textBoxLetter);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxNum;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLetter;
+        private System.Windows.Forms.Button buttonConvert;
+    }
+}
+
---
+++ PracticaDNI/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PracticaDNI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConvert_Click(object sender, EventArgs e)
+        {
+
+            int DNI = Convert.ToInt32(textBoxNum.Text);
+            char letter = functionDNI(DNI);
+            textBoxLetter.Text = letter.ToString();
+
+        }
+        private char functionDNI(int DNI)
+        {
+
+            char letter = 'A';
+
+
+
+            return letter;
+
+        }
+    }
+}
